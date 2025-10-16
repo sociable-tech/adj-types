2d2620bfd98ca896a80ca27f93b197f037496e51
Ghulam Dastgir <gdastgir@sociabletech.com.au>
2025-10-16 16:51:51 +0500

    feat: add 'dropdown' type to Question type definition

--- src/questionnaire/types.ts
+++ src/questionnaire/types.ts
@@ -31,7 +31,7 @@ export type Question = {
   parent: string | null
   section: string | null
   dependantAnswer: string | null
-  type: "multiple-choice" | "text" | "rating" | "file-or-text" | "file-upload" | "long-text" | "currency" | "date";
+  type: "multiple-choice" | "text" | "rating" | "file-or-text" | "file-upload" | "long-text" | "currency" | "date" | 'dropdown';
   options: string[] | null
 }
 
